6317af4f01c7fb0712406994f876ad3e6c8d17a7
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-05 13:30:45 -0700

    Finished Sort and Star and added solution

--- js/8kyu.js
+++ js/8kyu.js
@@ -20,7 +20,7 @@
 // Note: input will never be an empty string
 
 
-//MY SOLUTION
+// MY SOLUTION
 // const fakeBin = x => {
 //     let toArr = x.split("")
 //     let newArr = []
@@ -57,4 +57,26 @@
 
 
 
-//-------------------------------------------------------------------------------------------------------------------Fake Binary
+//-------------------------------------------------------------------------------------------------------------------Sort and Star
+
+// DESCRIPTION:
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+// MY SOLUTION
+
+// const twoSort = s => {
+//     let sortedArr = s.sort()
+//     console.log(sortedArr)
+//     return sortedArr[0].split("").join("***")
+// }
+
+// console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
+
+// OTHER SOLUTIONS
+// function twoSort(s) {
+//     return s.sort()[0].split('').join('***');
+//   }
