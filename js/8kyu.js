c91088320adfbd61365b4b5b12b9a7a4f105ecc6
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-05 13:18:45 -0700

    Did Fake Binary and added other solutions

--- js/8kyu.js
+++ js/8kyu.js
@@ -11,3 +11,50 @@
 // }
 
 // console.log(solution('world'))
+
+
+//-------------------------------------------------------------------------------------------------------------------Fake Binary
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+
+//MY SOLUTION
+// const fakeBin = x => {
+//     let toArr = x.split("")
+//     let newArr = []
+//     console.log(toArr)
+//     toArr.forEach((x,i) => {
+//         if(Number(x)<5){
+//             newArr.push(0)
+//         }else{
+//             newArr.push(1)}
+//     })
+//     return newArr.join("")
+// }
+
+// console.log(fakeBin('45385593107843568'))
+
+//THEIR SOLUTIONS
+
+// function fakeBin(x) {
+//     return x.split('').map(n => n < 5 ? 0 : 1).join('');
+// }
+
+// function fakeBin(str){
+//     var newStr = "";
+//     for(var i=0;i<str.length;i++){
+//       if(Number(str[i])>=5){
+//         newStr += "1"
+//       }
+//       else{
+//         newStr += "0";
+//       }
+//     }
+//     return newStr;
+//   }
+
+
+
+//-------------------------------------------------------------------------------------------------------------------Fake Binary
