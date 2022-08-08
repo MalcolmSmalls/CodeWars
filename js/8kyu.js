c1c91213a870e11aa6155a2097f9161c60d9efc1
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-08 11:28:29 -0700

    Working on Count by XX

--- js/8kyu.js
+++ js/8kyu.js
@@ -167,3 +167,154 @@
 //       return (n*n) + sum;
 //     }, 0)
 //   }
+
+//--------------------------------------------------------------------------------Thinkful - Number Drills: Blue and red marbles
+
+// You have a bunch of red and blue marbles. To start the game you grab a handful of marbles of each color and put them into the bag, keeping track of how many of each color go in. You take turns reaching into the bag, guessing a color, and then pulling one marble out. You get a point if you guessed correctly. The trick is you only have three seconds to make your guess, so you have to think quickly.
+
+// You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+
+// the number of blue marbles you put in the bag to start
+// the number of red marbles you put in the bag to start
+// the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+// the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+// guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+
+
+
+// const alternateCase = (str) => {
+//     let newStr = ""
+//     for(i=0; i < str.length; i++)
+//       if(str[i] === str[i].toUpperCase()){
+//         newStr += str[i].toLowerCase()
+//       }else{
+//         newStr += str[i].toUpperCase()
+//       }
+//       return str
+//   }
+
+//   const alternateCase = (str) => {
+//     let newStr = str.split('')
+//     const map = newStr.map((x) => {
+//         for(i=0;i< newStr.length; i++){
+//          if(x=== x.toUpperCase()){
+//             x = x.toLowerCase()
+//         }else{
+//             x = x.toUpperCase()
+//         }
+//         }
+//     })
+//     return map
+// }
+
+
+
+//   console.log(alternateCase("HELLO WORLD"))
+
+
+
+
+//-----------------------------------------------------------------------Merge two sorted arrays into one
+
+// DESCRIPTION:
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+// Happy coding!
+
+
+
+// const mergeArrays = (arr1, arr2) => {
+//   let newArr = []
+//   let noDupes = []
+//   arr1.forEach(x => newArr.push(x))
+//   arr2.forEach(x => newArr.push(x))
+//   newArr = newArr.sort((a,b)=>a-b)
+//   newArr.forEach((number) => {
+//       if (!noDupes.includes(number)){
+//           noDupes.push(number)
+//       }
+//   })
+//   return noDupes
+  
+// }
+
+// console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]))
+
+// OTHER SOLUTION
+// function mergeArrays(arr1, arr2) {
+//     return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+//   }
+
+
+
+//-----------------------------------------------------------------------Sum Mixed Array
+// DESCRIPTION:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+// const sumMix = x => {
+//     let toNum = []
+//     x.forEach(x=>toNum.push(Number(x)))
+//     return toNum.reduce((acc,curr)=> acc+curr,0)
+
+// }
+
+// console.log(sumMix([9, 3, '7', '3']))
+
+// OTHER SOLUTION
+
+// function sumMix(x){
+//     return x.map(a => +a).reduce((a, b) => a + b);
+//   }
+
+
+//--------------------------------------------------------------------Beginner Series #4 Cockroach
+
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+
+// function cockroachSpeed(s) {
+//     return Math.floor((s * 100000)/3600)
+//   }
+
+  // OTHER SOLUTIONS
+//   const cockroachSpeed = s => Math.floor(s / 0.036);
+
+//-------------------------------------------------------------------------------------------Count by XX
+
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+    let z = [];
+    for(i=0;i<n;i++){
+      z.push(x+n)}
+  
+    return z;
+  }
