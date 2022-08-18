a5b611902cf747ca659aab57ff1926b47664e10f
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-18 11:23:03 -0700

    Finished 10 CodeWar, If You Can't Sleep Just Count Sheep

--- js/8kyu.js
+++ js/8kyu.js
@@ -874,3 +874,40 @@
 // }
 
 // console.log(hexToDec('a'))
+
+
+//
+////
+//////
+//If you can't sleep, just count sheep!!-----------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// var countSheep = function (num){
+//   let sheep = ""
+//   if(num>0){
+//     for(i=1;i<=num;i++){
+//       sheep += `${i} sheep...`
+//     }
+//   }else{
+//     return ""
+//   }
+//   return sheep
+// }
+
+// console.log(countSheep(3))
+
+
+// OTHER SOLUTIONS
+// var countSheep = function (num){
+//   let str = "";
+//   for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+//   return str;
+// }
