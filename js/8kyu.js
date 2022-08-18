19118af4747f7d278e758e2770248de3b3fb6c57
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-18 10:48:33 -0700

    Finished 7th Codewar 'Count By X'

--- js/8kyu.js
+++ js/8kyu.js
@@ -780,12 +780,54 @@
 
 // The arguments will always be non empty strings, and normal letters.
 
-function correctTail(body, tail){
-  let sub = body.substr(body.length-(tail.length))
+// function correctTail(body, tail){
+//   let sub = body.substr(body.length-(tail.length))
   
-  if(sub === tail){
-    return true
-  }else{ 
-    return false
-  }
-}
+//   if(sub === tail){
+//     return true
+//   }else{ 
+//     return false
+//   }
+// }
+
+//
+////
+//////
+//Count by X----------------------------------------------------------------------------------------------------
+//////
+////
+//
+//
+
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// function countBy(x, n) {
+//   let arr = []
+//   for(i=1;i<=n;i++){
+//     arr.push(i)
+//   }
+//   const newArr = arr.map(num => num*x)
+//   return newArr
+// }
+
+// countBy(2,5)
+
+//OTHER SOLUTIONS//
+
+// function countBy(x, n) {
+//   var z = [];
+//   for (i = 1; i <= n; i++) {
+//       z.push(x * i);
+//   }
+//   return z;
+// }
