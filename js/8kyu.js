d938239eef2707b38437f3c28cc77bd319a9b255
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-17 20:57:30 -0700

    Finished opposite number

--- js/8kyu.js
+++ js/8kyu.js
@@ -475,3 +475,27 @@
 //  };
 
 //  console.log(removeChar('eloquent'))
+
+
+//Opposite number-----------------------------------------------------------------------------------------------
+
+
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+function opposite(number) {
+  if(number < 0){
+    return Math.abs(number)
+  }else{
+    return -number
+  }
+}
+
+console.log(opposite(4))
