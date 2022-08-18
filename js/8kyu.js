1ac41a54499be82a39af95a765e276b91a941958
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-18 10:05:54 -0700

    Finished 3rd Codewar Name Shuffler

--- js/8kyu.js
+++ js/8kyu.js
@@ -692,3 +692,27 @@
 // }
 
 // console.log(isPythagoreanTriple([3, 4, 5]))
+
+
+
+//
+////
+//////
+//Name Shuffler----------------------------------------------------------------------------------------------------------
+//////
+////
+//
+//
+
+// DESCRIPTION:
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+function nameShuffler(str){
+  return str.split(" ").reverse().join(" ")
+}
+
+console.log(nameShuffler('johnny bananas'))
