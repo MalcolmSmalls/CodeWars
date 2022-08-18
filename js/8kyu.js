e593fe45ae465cc50b87345d1f78ecfe7d729291
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-18 00:46:18 -0700

    Finished Lost Without A Map

--- js/8kyu.js
+++ js/8kyu.js
@@ -579,4 +579,32 @@
 //    return phrases[(n - 1) % phrases.length] 
 // }
 
-// [(i % 6)-1]
+
+
+//
+////
+//////
+//Beginner - Lost Without a Map-------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// function maps(x){
+//   let newArr = x.map(num => num*2)
+//   return newArr
+// }
+
+// console.log(maps([1, 2, 3]))
+
+//OTHER SOLUTION
+// function maps(x){
+//   return x.map(n => n * 2);
+// }
+
