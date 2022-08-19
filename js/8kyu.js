fd5e2ca88ea549816aec954d88e4b652485e8a56
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-19 09:59:26 -0700

    Finished 8th CodeWars'Remove String Spaces'

--- js/8kyu.js
+++ js/8kyu.js
@@ -1316,3 +1316,22 @@
 
 // OTHER SOLUTIONS
 // const quarterOf = m => Math.ceil(m/3);
+
+
+//
+////
+//////
+//Remove String Spaces-----------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// function noSpace(x){
+//   return x.replace(/ /gm, "")
+// }
+
+// console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
