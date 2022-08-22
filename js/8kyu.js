67249d54b65382c757710df2a6b07d48fe984903
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-22 11:09:02 -0700

    Finished find multiples of a number CodeWars

--- js/8kyu.js
+++ js/8kyu.js
@@ -1335,3 +1335,48 @@
 // }
 
 // console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
+
+//OTHER SOLUTIONS
+// function noSpace(x){
+//   return x.replace(/\s/g, '');
+// }
+
+
+
+//
+////
+//////
+//Find Multiples of a Number--------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+// For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+// If you can, try writing it in only one line of code.
+
+
+function findMultiples(integer, limit) {
+  let newArr = []
+  for(i=integer;i<=limit;i++){
+    if(i%integer===0){
+      newArr.push(i)
+    }
+  }
+  return newArr
+}
+
+console.log(findMultiples(2,6))
+
+//other solution
+// function findMultiples(int,limit){
+//   let result = []
+  
+//   for (let i = int; i<=limit ; i+=int)
+//     result.push(i)
+    
+//   return result
+// }
