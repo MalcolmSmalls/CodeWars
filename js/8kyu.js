ea21c2e038c841f509164db0e19e6a0c92e3bcdf
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-24 12:11:19 -0700

    Finished 'Opposites Attract'

--- js/8kyu.js
+++ js/8kyu.js
@@ -1437,8 +1437,35 @@
 
 
 
-function billboard(name, price = 30){
-  return name.split('').map(item => price).reduce((curr,acc) => curr + acc, 0)
-} 
+// function billboard(name, price = 30){
+//   return name.split('').map(item => price).reduce((curr,acc) => curr + acc, 0)
+// } 
 
-console.log(billboard("Werner Vígi",15))
+// console.log(billboard("Werner Vígi",15))
+
+
+
+//
+////
+//////
+//Opposites Attract---------------------------------------------------------------------------------
+//////
+////
+//
+
+
+
+// DESCRIPTION:
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0 ? true: false;
+}
+
+//other solution
+
+// function lovefunc(flower1, flower2){
+//   return flower1 % 2 !== flower2 % 2;
+// }
