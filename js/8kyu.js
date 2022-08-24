74a1865ff109b778ad65becdfdfbc5496a54ac87
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-24 13:08:40 -0700

    Finished 'Short Long Short' CodeWars

--- js/8kyu.js
+++ js/8kyu.js
@@ -1460,12 +1460,35 @@
 
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
-function lovefunc(flower1, flower2){
-  return flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0 ? true: false;
-}
+// function lovefunc(flower1, flower2){
+//   return flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0 ? true: false;
+// }
 
 //other solution
 
 // function lovefunc(flower1, flower2){
 //   return flower1 % 2 !== flower2 % 2;
 // }
+
+
+//
+////
+//////
+//Short Long Short-------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+const solution = (a, b) => a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
