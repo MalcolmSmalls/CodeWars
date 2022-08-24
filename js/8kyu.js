4e418b1a773263a5d504cb5fe062d2bcfb700d07
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-24 11:58:06 -0700

    Finished 'Name on Billboard' codeWars 1

--- js/8kyu.js
+++ js/8kyu.js
@@ -1416,4 +1416,29 @@
 // }
 
 
-git commit -m "Finished CodeWars 1 'Find numbers which are divisible by given number'"
+//
+////
+//////
+//Name on billboard---------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+// You can not use multiplier "*" operator.
+
+// If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
+
+// function billboard(name, price = 30){
+//   return name.length * price
+// } 
+
+
+
+function billboard(name, price = 30){
+  return name.split('').map(item => price).reduce((curr,acc) => curr + acc, 0)
+} 
+
+console.log(billboard("Werner Vígi",15))
