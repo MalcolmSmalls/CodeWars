697e83aa24a771682266205265198268791a1b13
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-24 22:07:03 -0700

    Finished 'Geometry Basics: Distance between points in 2D' CodeWar

--- js/8kyu.js
+++ js/8kyu.js
@@ -1643,3 +1643,43 @@
 //   return good_count < 1 ? 'Fail!' : 
 //          good_count < 3 ? 'Publish!' : 'I smell a series!';
 // }
+
+
+//
+////
+//////
+//Geometry Basics: Distance between points in 2D--------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x and y attributes (X and Y in C#) attributes.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests round answers to 6 decimal places.
+
+
+// class Point{
+//   constructor(x,y){
+//   this.x = x
+//   this.y = y
+//   }
+// }
+
+// function distanceBetweenPoints(a, b) {
+//   let aH = a
+//   let bH = b
+//   return Math.sqrt((bH.x - aH.x)**2 + (bH.y-aH.y)**2)
+// }
+
+// OTHER SOLUTION
+
+// function distanceBetweenPoints(a, b) {
+//   return Math.hypot(a.x - b.x, a.y - b.y);
+// }
+
