b42c60fbd5ba62fe6c1e57b10973bb48b2599f25
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-24 18:48:56 -0700

    Finished CodeWar 'Safen User Input Part I - htmlspecialchars'

--- js/8kyu.js
+++ js/8kyu.js
@@ -1491,4 +1491,57 @@
 // ("1", "22") --> "1221"
 // ("22", "1") --> "1221"
 
-const solution = (a, b) => a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
+// const solution = (a, b) => a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
+
+
+//
+////
+//////
+//Safen User Input Part I - htmlspecialchars-------------------------------------------------------------------
+//////
+////
+//
+
+
+// DETAILS:
+// You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+
+// Mission
+// Your mission is to implement a function that converts the following potentially harmful characters:
+
+// < --> &lt;
+// > --> &gt;
+// " --> &quot;
+// & --> &amp;
+// Good luck :D
+
+
+
+const htmlspecialchars = formData => {
+  newArr = []
+  formData.split("").forEach(x=>{
+  if(x === "<"){
+    newArr.push("&lt;")
+  }else if(x===">"){
+    newArr.push("&gt;")
+  }else if(x==="&"){
+    newArr.push("&amp;")
+  }else if(x==='"'){
+    newArr.push("&quot;")
+  }else{
+    newArr.push(x)
+  }
+})
+  return newArr.join("")
+}
+
+console.log(htmlspecialchars("<h2>Hello World</h2>"))
+
+//other solutions 
+
+// function htmlspecialchars(formData) {
+//   return formData.replace(/&/g, "&amp;")
+//                  .replace(/"/g, "&quot;")
+//                  .replace(/</g, "&lt;")
+//                  .replace(/>/g, "&gt;");
+// }
