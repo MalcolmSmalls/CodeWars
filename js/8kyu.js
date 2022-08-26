e2e3bfe03f2baacb38db247312e7b7106e006ad7
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-25 21:56:29 -0700

    Finished 'Grasshopper - Summation' CodeWars

--- js/8kyu.js
+++ js/8kyu.js
@@ -1729,3 +1729,41 @@
 
 // Example:
 // move(3, 6) should equal 15
+
+
+// function move (position, roll) {
+//   return roll*2 + position
+// }
+
+
+
+//
+////
+//////
+//Grasshopper - Summation----------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+var summation = function (num) {
+  let count = 0
+  for(i=1;i<=num;i++){
+    count = i+count
+  }
+  return count
+}
+
+console.log(summation(8))
