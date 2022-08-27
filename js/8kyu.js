9740b347de74452d67b2f1de050866e056c686a5
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-27 13:36:26 -0700

    Finish 'N-th Power' CodeWar

--- js/8kyu.js
+++ js/8kyu.js
@@ -2362,22 +2362,50 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // "AB","Ab"     -> false
 // "",""         -> false
 
-function isOpposite(s1,s2){
-  let reversed = ""
-  for(i=0;i<s1.length;i++){
-    if(s1[i] === s1[i].toUpperCase()){
-      reversed += s1[i].toLowerCase()
-    }else{
-      reversed += s1[i].toUpperCase()
-    }
-  }
-  if(reversed === "" || reversed !== s2){
-    return false
-  }else{
-    return true
-  }
-}
+// function isOpposite(s1,s2){
+//   let reversed = ""
+//   for(i=0;i<s1.length;i++){
+//     if(s1[i] === s1[i].toUpperCase()){
+//       reversed += s1[i].toLowerCase()
+//     }else{
+//       reversed += s1[i].toUpperCase()
+//     }
+//   }
+//   if(reversed === "" || reversed !== s2){
+//     return false
+//   }else{
+//     return true
+//   }
+// }
 
 
 
-console.log(isOpposite("aBcd","AbCD"))
+// console.log(isOpposite("aBcd","AbCD"))
+
+
+
+//
+////
+//////
+//N-th Power-----------------------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+// Let's look at a few examples:
+
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+// function index(array, n){
+//   if(array[n] === undefined){
+//     return -1
+//   }else{
+//   return array[n]**n
+//   }
+// }
