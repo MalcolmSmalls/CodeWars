63633f92336dd292b54376e30a37b565a8584f26
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-27 11:50:04 -0700

    Finish 'CSV representation of array' CodeWar

--- js/8kyu.js
+++ js/8kyu.js
@@ -2242,3 +2242,58 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // Waiting for translations and Feedback! Thanks!
 
 // const paperwork = (n,m) => n <= 0 || m <= 0 ? 0 : n*m
+
+
+
+//
+////
+//////
+//CSV representation of array-----------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+
+function toCsvText(array) {
+  let holder = ""
+  array.forEach(x => {
+    if(x === array[array.length-1]){
+      holder += `${x}`
+    }else{
+      holder += `${x}\n`
+    }})
+  return holder
+}
+
+console.log(toCsvText([
+  [ 5,55,5,5,55 ],
+  [ 6,6,66,23,24 ],
+  [ 666,31,66,33,7 ]
+ ] ))
+
+ // OTHER SOLUTIONS
+
+//  function toCsvText(array) {
+//   return array.join('\n');
+// }
