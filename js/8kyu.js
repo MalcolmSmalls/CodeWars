41e3ee92f8a2f53e9b213241b58efafd8f08b057
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-27 13:23:32 -0700

    Finished 4th CodeWar
    They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?

--- js/8kyu.js
+++ js/8kyu.js
@@ -2335,6 +2335,49 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // Notes
 // String str will never be null.
 
-var replaceDots = function(str) {
-  return str.replace(/\./g, '-');
+// var replaceDots = function(str) {
+//   return str.replace(/\./g, '-');
+// }
+
+
+//
+////
+//////
+//They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?-----------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Task
+// Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+
+// The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+
+// Examples (input -> output)
+// "ab","AB"     -> true
+// "aB","Ab"     -> true
+// "aBcd","AbCD" -> true
+// "AB","Ab"     -> false
+// "",""         -> false
+
+function isOpposite(s1,s2){
+  let reversed = ""
+  for(i=0;i<s1.length;i++){
+    if(s1[i] === s1[i].toUpperCase()){
+      reversed += s1[i].toLowerCase()
+    }else{
+      reversed += s1[i].toUpperCase()
+    }
+  }
+  if(reversed === "" || reversed !== s2){
+    return false
+  }else{
+    return true
+  }
 }
+
+
+
+console.log(isOpposite("aBcd","AbCD"))
