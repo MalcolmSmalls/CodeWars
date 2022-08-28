288147fc69b8de973841a03d4ed62fbd411bab8a
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-28 09:20:07 -0700

    Finish 3rd CodeWar 'Rock, Paper, Scissors'

--- js/8kyu.js
+++ js/8kyu.js
@@ -2558,19 +2558,52 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 // This Cube class needs help
 // Implement the constructor so that it can take an integer for the side or no args
-class Cube {
-  constructor(n){
-    this.n = n * 4
-    this.side= Math.abs(n)
-  }
-  getSide() {
-    if(Number.isInteger(this.side)){
-      return this.side; 
-    }else{
-      return 0
-    }
-  }
-  setSide(n) {
-    this.side = Math.abs(n);
-  }
-}
+// class Cube {
+//   constructor(n){
+//     this.n = n * 4
+//     this.side= Math.abs(n)
+//   }
+//   getSide() {
+//     if(Number.isInteger(this.side)){
+//       return this.side; 
+//     }else{
+//       return 0
+//     }
+//   }
+//   setSide(n) {
+//     this.side = Math.abs(n);
+//   }
+// }
+
+//
+////
+//////
+//Rock Paper Scissors!------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// const rps = (p1, p2) => p1 === 'rock' && p2 === 'paper' || p1 === 'paper' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'rock' ? 'Player 2 won!' : p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock' || p1 === 'scissors' && p2 === 'paper' ? 'Player 1 won!' : 'Draw!'
+
+// OTHER SOLUTIONS
+
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//   if (p2 === rules[p1]) {
+//     return "Player 1 won!";
+//   }
+//   else {
+//     return "Player 2 won!";
+//   }
+// };
