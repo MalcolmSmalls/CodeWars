cacfba3e0ffcabfaf07184bb11228d8b3e687975
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-28 09:10:05 -0700

    Finish 2nd CodeWar 'Playing with Cubes'

--- js/8kyu.js
+++ js/8kyu.js
@@ -2510,7 +2510,7 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //
 ////
 //////
-//Will there be enough space?---------------------------------------------------------------------------------------------------------
+//Will there be enough space?--------------------------------------------------------------------------------------------------
 //////
 ////
 //
@@ -2533,6 +2533,44 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 
 
-const enough = (cap, on, wait) => on + wait > cap ? Math.abs(cap - (on+wait)) : 0
+// const enough = (cap, on, wait) => on + wait > cap ? Math.abs(cap - (on+wait)) : 0
 
-console.log(enough(10, 5, 6))
+// console.log(enough(10, 5, 6))
+
+
+//
+////
+//////
+//Playing with cubes II-------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Hey Codewarrior!
+
+// You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+
+// Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+
+// The constructor taking no arguments should assign 0 to Cube's Side property.
+
+// This Cube class needs help
+// Implement the constructor so that it can take an integer for the side or no args
+class Cube {
+  constructor(n){
+    this.n = n * 4
+    this.side= Math.abs(n)
+  }
+  getSide() {
+    if(Number.isInteger(this.side)){
+      return this.side; 
+    }else{
+      return 0
+    }
+  }
+  setSide(n) {
+    this.side = Math.abs(n);
+  }
+}
