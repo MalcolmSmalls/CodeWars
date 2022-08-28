42cc2957e24a3d0905a74502c6effb8eae14c369
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-28 11:13:57 -0700

    Finish 8th CodeWar 'Total amount of points'

--- js/8kyu.js
+++ js/8kyu.js
@@ -2707,7 +2707,7 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //
 ////
 //////
-//Compare within margin
+//Compare within margin-------------------------------------------------------------------------------------------------
 //////
 ////
 //
@@ -2769,3 +2769,48 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // function closeCompare(a, b, m = 0){
 //   return Math.abs(a - b) <= m? 0: Math.sign(a - b);
 // }
+
+
+//
+////
+//////
+//Total amount of points---------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
+// function points(games) {
+//   let results = 0
+//   games.forEach(x => {    
+//     if(x[0] > x[2]){
+//       results +=3
+//     }else if(x[0] === x[2]){
+//       results += 1
+//     }
+//   })
+//   return results
+// }
+
+
+// OTHER SOLUTION
+
+// const points=games=>games.reduce((output,current)=>{
+//   return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+// },0)
