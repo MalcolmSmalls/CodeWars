ecd903d51c77a968ee31976203e063ab06039673
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-29 08:31:14 -0700

    Finish 2nd CodeWar 'A wolf in sheep's clothing'

--- js/8kyu.js
+++ js/8kyu.js
@@ -2828,4 +2828,91 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // DESCRIPTION:
 // There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
 
-Object.freeze(MrFreeze)
+// Object.freeze(MrFreeze)
+
+
+
+//
+////
+//////
+//Coefficients of the Quadratic Equation-----------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+// Equation will be the form of ax^2 + bx + c = 0
+
+// Return type is a Vector (tuple in Rust, Array in Ruby) containing coefficients of the equations in the order (a, b, c).
+
+// Since there are infinitely many solutions to this problem, we fix a = 1.
+
+// Remember, the roots can be written like (x-x1) * (x-x2) = 0
+
+// Example
+// quadratic(1,2) = (1, -3, 2)
+// This means (x-1) * (x-2) = 0; when we do the multiplication this becomes x^2 - 3x + 2 = 0
+
+// Example 2
+// quadratic(0,1) = (1, -1, 0)
+// This means (x-0) * (x-1) = 0; when we do the multiplication this becomes x^2 - x + 0 = 0
+
+// Notes
+// Inputs will be integers.
+// When x1 == x2, this means the root has the multiplicity of two
+
+
+// function quadratic(x1, x2){
+//   let results = [1]
+
+//   }
+
+
+//
+////
+//////
+//A wolf in sheep's clothing----------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+function warnTheSheep(queue) {
+  let message = ""
+  queue.forEach((x,i) =>{
+    if(x==='wolf' && i !== queue.length-1){
+      message = `Oi! Sheep number ${queue.length-(i+1)}! You are about to be eaten by a wolf!`
+    }else if(x==='wolf' && i === queue.length-1){
+      message = "Pls go away and stop eating my sheep"   
+    }
+  })
+  return message
+}
+
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]))
+
+
+// OTHER SOLUTIONS
+
+// function warnTheSheep(queue) {
+//   const position = queue.reverse().indexOf('wolf');
+//   return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+// }
