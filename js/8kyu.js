b94d1dc1a045dd0b4a1ee9078e9a2966cf617976
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-29 09:36:00 -0700

    Finish 5th CodeWar, 'Pillars'

--- js/8kyu.js
+++ js/8kyu.js
@@ -2958,3 +2958,26 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // function buildString(...template){
 //   return `I like ${template.join(', ')}!`;
 // }
+
+
+//
+////
+//////
+//Pillars-------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+// const pillars = (numPill, dist, width) =>
+//   numPill === 1 ? 0 : (dist*100)*(numPill-1)+(width*(numPill-2))
+
+
