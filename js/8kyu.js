c78344cb8be4e0ddc79b4308474a45f37d4bb9d1
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-30 20:44:05 -0700

    Finished CodeWar 1: 'Reversed Words'

--- js/8kyu.js
+++ js/8kyu.js
@@ -3109,3 +3109,22 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // };
 
 
+
+//
+////
+//////
+//Reversed Words------------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ")
+}
