9e75e5b0b7aad7c8e46dff9bebe090bec7926fe0
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-30 22:10:11 -0700

    Work on 'Subtract the Sum' CodeWar

--- js/8kyu.js
+++ js/8kyu.js
@@ -3125,6 +3125,56 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
-function reverseWords(str){
-  return str.split(" ").reverse().join(" ")
+// function reverseWords(str){
+//   return str.split(" ").reverse().join(" ")
+// }
+
+
+//
+////
+//////
+//Subtract the Sum------------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// Subtract the sum
+// NOTE! This kata can be more difficult than regular 8-kyu katas (lets say 7 or 6 kyu)
+
+// Complete the function which get an input number n such that n >= 10 and n < 10000, then:
+
+// Sum all the digits of n.
+// Subtract the sum from n, and it is your new n.
+// If the new n is in the list below return the associated fruit, otherwise return back to task 1.
+// Example
+// n = 325
+// sum = 3+2+5 = 10
+// n = 325-10 = 315 (not in the list)
+// sum = 3+1+5 = 9
+// n = 315-9 = 306 (not in the list)
+// sum = 3+0+6 = 9
+// n =306-9 = 297 (not in the list)
+// .
+// .
+// .
+// ...until you find the first n in the list below.
+
+// There is no preloaded code to help you. This is not about coding skills; think before you code
+
+
+const list = ['kiwi', 'pear', 'kiwi', 'banana', 'melon', 'banana', 'melon', 'pineapple', 'apple', 'pineapple', 'cucumber','pineapple', 'cucumber', 'orange', 'grape', 'orange', 'grape', 'apple', 'grape', 'cherry', 'pear', 'cherry', 'pear', 'kiwi', 'banana', 'kiwi', 'apple', 'melon', 'banana', 'melon', 'pineapple', 'melon', 'pineapple', 'cucumber', 'orange', 'apple', 'orange', 'grape', 'orange', 'grape', 'cherry', 'pear', 'cherry', 'pear', 'apple', 'pear', 'kiwi', 'banana', 'kiwi', 'banana', 'melon', 'pineapple', 'melon', 'apple', 'cucumber', 'pineapple', 'cucumber', 'orange', 'cucumber', 'orange', 'grape', 'cherry', 'apple', 'cherry', 'pear', 'cherry', 'pear', 'kiwi', 'pear', 'kiwi', 'banana', 'apple', 'banana', 'melon', 'pineapple', 'melon','pineapple', 'cucumber', 'pineapple', 'cucumber', 'apple', 'grape', 'orange', 'grape', 'cherry', 'grape', 'cherry', 'pear', 'cherry', 'apple', 'kiwi', 'banana', 'kiwi', 'banana', 'melon', 'banana', 'melon', 'pineapple', 'apple', 'pineapple']
+
+function SubtractSum(n){
+  let results = 0
+  let newArr = n.toString().split("")
+  let firstNum = n.toString().split("")
+  newArr.forEach(x => results += parseInt(x))
+  let newN = list[n-results-1]
+  if(newN !== undefined){
+    return newN
+  }else{
+    return list[parseInt(firstNum[0]-1)]
+  }
 }
+
+console.log(SubtractSum(200))
