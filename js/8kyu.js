14b3765dfc571ae195df35fd90cf6653ab3dbfa0
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-01 11:55:19 -0700

    Finish 5th CodeWar, 'Remove exclamation marks'

--- js/8kyu.js
+++ js/8kyu.js
@@ -3319,3 +3319,19 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //OTHER SOLUTION
 
 //const remove = s => s.replace(/!$/, '');
+
+
+//
+////
+//////
+//Remove exclamation marks----------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// function removeExclamationMarks(s) {
+//   return s.replace(/!/g,"");
+// }
