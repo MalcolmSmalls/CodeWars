d960d3f112566f04117ab7823ce0c0ed956c09b9
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-07 12:46:36 -0700

    Finish 3rd CodeWar 'Counting Duplicates'

--- js/8kyu.js
+++ js/8kyu.js
@@ -3404,3 +3404,81 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //   // i will be an integer. Double it and return it.
 //   return i*2
 // }
+
+
+
+
+
+//
+////
+//////
+//Counting Duplicates-------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+
+
+
+
+
+// function duplicateCount(text){
+//   let holdLetter = ""
+//   let holdArr = []
+//   let textToArray = text.toLowerCase().split("").sort()
+//   console.log(textToArray)
+//   textToArray.forEach((letter, index) => {
+//     index++
+//     holdLetter = textToArray[index]
+//     if(letter === holdLetter){
+//       holdArr.push(letter)
+//     }
+//   })
+//   holdArr.forEach((letter, index) => {
+//     index++
+//     holdLetter = holdArr[index]
+//     if(letter === holdLetter){
+//       holdArr.shift()
+//     }
+//   })
+//   return holdArr
+// }
+
+
+function duplicateCount(text){
+  let strToArr = text.toLowerCase().split("")
+  let count = 0
+  let newArr = strToArr.filter((char, index) => {
+    return text.indexOf(char) !== index
+  })
+  newArr.forEach((char,index)=> {
+    if(index === newArr.indexOf(char)){
+      count++
+    }
+  })
+  return count
+}
+
+console.log(duplicateCount('Indivisibilities'))
+
+
+// other solution
+
+// function duplicateCount(text){
+//   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+// }
