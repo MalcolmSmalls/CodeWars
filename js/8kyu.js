ec9ba2a745310de9324b9bdca6b54ab7a8405f71
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-07 01:14:56 -0700

    Finish 2nd CodeWar  'You Can't Code Under Pressure #1'

--- js/8kyu.js
+++ js/8kyu.js
@@ -3387,3 +3387,20 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
   
 //   return name;
 // }
+
+
+//
+////
+//////
+//You Can't Code Under Pressure #1------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// function doubleInteger(i) {
+//   // i will be an integer. Double it and return it.
+//   return i*2
+// }
