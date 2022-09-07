de3eb041748525d713132bd6f86243a303c169b4
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-07 01:11:22 -0700

    Finish 1st CodeWar 'Get Planet Name By ID'

--- js/8kyu.js
+++ js/8kyu.js
@@ -3335,3 +3335,55 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // function removeExclamationMarks(s) {
 //   return s.replace(/!/g,"");
 // }
+
+
+
+
+//
+////
+//////
+//Get Planet Name By ID-----------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+
+// function getPlanetName(id){
+//   var name;
+//   switch(id){
+//     case 1:
+//       name = 'Mercury'
+//       break;
+//     case 2:
+//       name = 'Venus'
+//       break;
+//     case 3:
+//       name = 'Earth'
+//       break;
+//     case 4:
+//       name = 'Mars'
+//       break;
+//     case 5:
+//       name = 'Jupiter'
+//       break;
+//     case 6:
+//       name = 'Saturn'
+//       break;
+//     case 7:
+//       name = 'Uranus'
+//       break;
+//     case 8:
+//       name = 'Neptune'
+//       break;
+//   }
+  
+//   return name;
+// }
