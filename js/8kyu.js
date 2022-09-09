0b5894fdc825745207e48ed4bf21c14bdfb82b8e
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-09 13:47:39 -0700

    Finish 5th CodeWar "Descending Order"

--- js/8kyu.js
+++ js/8kyu.js
@@ -4063,4 +4063,26 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
 
-const likes = names => names.length === 0 ? `no one likes this` : names.length === 1 ? `${names} likes this` : names.length === 2 ? `${names[0]} and ${names[1]} like this` : names.length === 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this` : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+// const likes = names => names.length === 0 ? `no one likes this` : names.length === 1 ? `${names} likes this` : names.length === 2 ? `${names[0]} and ${names[1]} like this` : names.length === 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this` : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+
+
+//
+////
+//////
+//Descending Order------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+  return Number(n.toString().split("").sort((a,b) => b-a).join(""))}
