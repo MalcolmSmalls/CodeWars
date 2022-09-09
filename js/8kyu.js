67b26db711c21e4f5d546eb192cd7064b2f3a997
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-09 13:28:10 -0700

    Finish 4th CodeWar 'Who likes it?'

--- js/8kyu.js
+++ js/8kyu.js
@@ -4018,26 +4018,49 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //   return newArr
 // }
 
-function arrayDiff(a, b) {
-  let newArr = []
-  if(a.length === 0){
-    return a
-  }else if(b.length === 0){
-    return a
-  }else{
-    a.filter(number => {
-      if(b.includes(number) === false){
-        newArr.push(number)
-      }
-    })
-  }  
-  return newArr
-}
+// function arrayDiff(a, b) {
+//   let newArr = []
+//   if(a.length === 0){
+//     return a
+//   }else if(b.length === 0){
+//     return a
+//   }else{
+//     a.filter(number => {
+//       if(b.includes(number) === false){
+//         newArr.push(number)
+//       }
+//     })
+//   }  
+//   return newArr
+// }
 
-console.log(arrayDiff([1,2,3], [1,2]))
+// console.log(arrayDiff([1,2,3], [1,2]))
 
 // OTHER SOLUTIONS
 
 // function array_diff(a, b) {
 //   return a.filter(e => !b.includes(e));
 // }
+
+//
+////
+//////
+//Who likes it?-----------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+const likes = names => names.length === 0 ? `no one likes this` : names.length === 1 ? `${names} likes this` : names.length === 2 ? `${names[0]} and ${names[1]} like this` : names.length === 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this` : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
