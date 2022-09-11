991127cc16db9057f4aff738a47a44becb281a81
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-11 14:01:51 -0700

    Finish 5th CodeWar 'Find The Parity Outlier'

--- js/8kyu.js
+++ js/8kyu.js
@@ -4458,7 +4458,56 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-function filter_list(l) {
-  return l.filter(item => typeof(item) === 'number')
+// function filter_list(l) {
+//   return l.filter(item => typeof(item) === 'number')
+//   }
+// console.log(filter_list([1,'a','b',0,15]))
+
+
+
+//
+////
+//////
+//Find The Parity Outlier------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+
+function findOutlier(integers){
+  let newArrEven = []
+  let newArrOdd = []
+  integers.forEach(item => {
+    if(item % 2 === 0){
+      newArrEven.push(item)
+    }else{
+      newArrOdd.push(item)
+    }
+  })
+  if(newArrOdd.length > newArrEven.length){
+    return Number(newArrEven)
+  }else{
+    return Number(newArrOdd)
   }
-console.log(filter_list([1,'a','b',0,15]))
+}
+
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
+
+// OTHER SOLUTION
+
+// function findOutlier(int){
+//   var even = int.filter(a=>a%2==0);
+//   var odd = int.filter(a=>a%2!==0);
+//   return even.length==1? even[0] : odd[0];
+// }
+
