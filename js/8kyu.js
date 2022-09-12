6297f5743e2faf0bbdef813a042342a0ed6e22fe
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-12 14:20:50 -0700

    Finish 6th CodeWar 'Jaden Casing Strings'

--- js/8kyu.js
+++ js/8kyu.js
@@ -4768,7 +4768,39 @@ function DNAStrand(dna){
 
 
 
-function sumTwoSmallestNumbers(numbers) {  
-  numbers.sort((a,b) => a-b)
-  return numbers[0] + numbers[1]
-}
+// function sumTwoSmallestNumbers(numbers) {  
+//   numbers.sort((a,b) => a-b)
+//   return numbers[0] + numbers[1]
+// }
+
+
+
+//
+////
+//////
+//Jaden Casing Strings------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+String.prototype.toJadenCase = function () {
+  let holderArr = this.split(" ").map(item =>{
+    return item.slice(0,1).toUpperCase() + item.substring(1)}
+  )
+  return holderArr.join(" ")
+};
+
+var str = "How can mirrors be real if our eyes aren't real"
+str.toJadenCase()
