0c28631b6e76c6bcb4e0fd678694a281c5c09439
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-12 13:08:32 -0700

    Finish 1st CodeWar 'Persistent Bugger'

--- js/8kyu.js
+++ js/8kyu.js
@@ -4484,24 +4484,24 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // Should return: 160 (the only even number)
 
 
-function findOutlier(integers){
-  let newArrEven = []
-  let newArrOdd = []
-  integers.forEach(item => {
-    if(item % 2 === 0){
-      newArrEven.push(item)
-    }else{
-      newArrOdd.push(item)
-    }
-  })
-  if(newArrOdd.length > newArrEven.length){
-    return Number(newArrEven)
-  }else{
-    return Number(newArrOdd)
-  }
-}
+// function findOutlier(integers){
+//   let newArrEven = []
+//   let newArrOdd = []
+//   integers.forEach(item => {
+//     if(item % 2 === 0){
+//       newArrEven.push(item)
+//     }else{
+//       newArrOdd.push(item)
+//     }
+//   })
+//   if(newArrOdd.length > newArrEven.length){
+//     return Number(newArrEven)
+//   }else{
+//     return Number(newArrOdd)
+//   }
+// }
 
-console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
+// console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
 
 // OTHER SOLUTION
 
@@ -4511,3 +4511,85 @@ console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
 //   return even.length==1? even[0] : odd[0];
 // }
 
+
+
+//
+////
+//////
+//Shortest Word-------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// function findShort(s){
+//   let newArr = s.split(" ")
+//   let shortestWord = newArr[0]
+//   newArr.forEach(word => {
+//     if(word.length < shortestWord.length){
+//       shortestWord = word
+//     }
+//   })
+//   return shortestWord.length
+// }
+
+
+//
+////
+//////
+//Persistent Bugger.-----------------------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+
+function persistence(num) {
+  let holderArr = num.toString().split("").map(number => +number)
+  let holder = holderArr.reduce((acc,total) => acc*total,1)
+  for(i=1;i<10;i++){
+    if(num < 10){
+    return 0
+    }else if(holder < 10){
+      return i
+    }else{
+      holderArr = holder.toString().split("").map(number=>+number)
+      holder = holderArr.reduce((acc,total) => acc*total,1)
+      if(holder < 10){
+        return i+1
+      }
+    }
+  }
+
+}
+
+console.log((persistence(5127140)))
+
+
+// OTHER SOLUTIONS
+
+// function persistence(num) {
+//   var times = 0;
+  
+//   num = num.toString();
+  
+//   while (num.length > 1) {
+//     times++;
+//     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//   }
+  
+//   return times;
+// }
