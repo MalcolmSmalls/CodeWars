4569d2af445622d3bb850f8594eba2a15965dc1f
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-03 13:18:46 -0700

    Finish CodeWars 8: Get the mean of an array

--- js/8kyu.js
+++ js/8kyu.js
@@ -5126,10 +5126,10 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 // P.S. Each array includes only integer numbers. Output is a number too.
 
-function arrayPlusArray(arr1, arr2) {
-  let newArr = [arr1, arr2]
-  return newArr.flatMap(item => item).reduce((prev, curr) => prev+curr,0)
-}
+// function arrayPlusArray(arr1, arr2) {
+//   let newArr = [arr1, arr2]
+//   return newArr.flatMap(item => item).reduce((prev, curr) => prev+curr,0)
+// }
 
 
 // OTHER SOLUTION 
@@ -5137,3 +5137,23 @@ function arrayPlusArray(arr1, arr2) {
 // function arrayPlusArray(arr1, arr2) {
 //     return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
 //   }
+
+
+//
+////
+//////
+//Get the mean of an array---------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((prev,curr) => prev+curr,0) / marks.length)
+}
