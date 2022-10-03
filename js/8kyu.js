e753f20b8dae3de86c91fd33f4df1e69604c04d8
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-03 11:03:53 -0700

    Finish CodeWars 3: A Needle in the Haystack

--- js/8kyu.js
+++ js/8kyu.js
@@ -4996,7 +4996,47 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 
 
-const basicOp = (operation, value1, value2) => operation === "+" ? value1 + value2 : operation === "-" ? value1 - value2 : operation === "*" ? value1 * value2 : operation === "/" ? value1 / value2 : 0
+// const basicOp = (operation, value1, value2) => operation === "+" ? value1 + value2 : operation === "-" ? value1 - value2 : operation === "*" ? value1 * value2 : operation === "/" ? value1 / value2 : 0
 
 
-console.log(basicOp('*', 49, 7))
+// console.log(basicOp('*', 49, 7))
+
+
+//
+////
+//////
+//A Needle in the Haystack------------------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// Note: In COBOL, it should return "found the needle at position 6"
+
+function findNeedle(haystack) {
+  let res = 0
+  haystack.forEach((item,index)=> {
+    if(item==='needle'){
+      res = index
+    }})
+    return `found the needle at position ${res}`
+}
+
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
+
+// OTHER SOLUTIONS:
+
+// function findNeedle(haystack) {
+//   return "found the needle at position " + haystack.indexOf("needle");
+// }
