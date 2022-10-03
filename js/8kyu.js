0400520699b6993eca24c1cc4ff9aa6b069ae478
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-03 10:49:53 -0700

    Finish CodeWars 2: DNA to RNA Conversion

--- js/8kyu.js
+++ js/8kyu.js
@@ -4955,18 +4955,48 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
-function DNAtoRNA(dna) {
-  let newArr = dna.split('')
-  let switchedArr = newArr.map(item => {
-    if(item==='T'){
-      item='U'
-      return item
-    }else{
-      return item
-    }
-  })
-  return switchedArr.join('')
+// function DNAtoRNA(dna) {
+//   let newArr = dna.split('')
+//   let switchedArr = newArr.map(item => {
+//     if(item==='T'){
+//       item='U'
+//       return item
+//     }else{
+//       return item
+//     }
+//   })
+//   return switchedArr.join('')
 
-}
+// }
 
-console.log(DNAtoRNA('GCAT'))
+// console.log(DNAtoRNA('GCAT'))
+
+
+//
+////
+//////
+//DNA to RNA Conversion-------------------------------------------------------------------------------------
+//////
+////
+//
+
+
+
+// DESCRIPTION:
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+
+const basicOp = (operation, value1, value2) => operation === "+" ? value1 + value2 : operation === "-" ? value1 - value2 : operation === "*" ? value1 * value2 : operation === "/" ? value1 / value2 : 0
+
+
+console.log(basicOp('*', 49, 7))
