bf8c9c4de653317d7d6e4e104ecd9ebad776408f
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-04 15:11:07 -0700

    Finish CodeWars 2: Grasshopper - Grade book

--- js/8kyu.js
+++ js/8kyu.js
@@ -5178,13 +5178,13 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //  1 --> [1]
 
 
-function monkeyCount(n){
-  let newArr = []
-  for(i=1;i<=n; i++){
-    newArr.push(i)
-  }
-  return newArr
-}
+// function monkeyCount(n){
+//   let newArr = []
+//   for(i=1;i<=n; i++){
+//     newArr.push(i)
+//   }
+//   return newArr
+// }
 
 
 
@@ -5199,3 +5199,48 @@ function monkeyCount(n){
 // }
 
 // console.log(monkeyCount(3))
+
+
+//
+////
+//////
+//Grasshopper - Grade book--------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+
+// const getGrade = (s1,s2,s3) => {
+//   let avg = (s1+s2+s3) / 3
+//   console.log(avg)
+//   return avg >= 90 ? "A" : avg >= 80 && avg < 90 ? "B" : avg >= 70 && avg < 80 ? "C" : avg >= 60 && avg < 70 ? "D" : "F" 
+// }
+
+
+// OTHER SOLUTIONS
+
+// const grades = { A: 90, B: 80, C: 70, D: 60, F: 0 };
+// const sum    = (sum, item)  => sum + item;
+// const mean   = (...numbers) => numbers.reduce(sum, 0) / numbers.length;
+
+// function getGrade () {
+//   const score = mean(...arguments);
+  
+//   for (key in grades) {
+//     if (!grades.hasOwnProperty(key)) continue; // Never do a for-in without this
+//     if (score >= grades[key]) return key; 
+//   }
+// }
