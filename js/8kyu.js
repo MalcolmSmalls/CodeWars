0277b90835d54cdd73fcde999b0b1ba5a0ebd268
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-04 16:08:27 -0700

    Finish CodeWars 3: The Feast of Many Beasts

--- js/8kyu.js
+++ js/8kyu.js
@@ -5244,3 +5244,30 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 //     if (score >= grades[key]) return key; 
 //   }
 // }
+
+
+//
+////
+//////
+//The Feast of Many Beasts----------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+  if(beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)){
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log(feast("great blue heron", "garlic naan"))
