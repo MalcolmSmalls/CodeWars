69a94d35f9a6e3feaec084a2037626ccd28ab3a3
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-04 12:35:56 -0700

    Finish CodeWars 1: Count the Monkeys!

--- js/8kyu.js
+++ js/8kyu.js
@@ -5154,6 +5154,48 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 // The array will never be empty.
 
-function getAverage(marks){
-  return Math.floor(marks.reduce((prev,curr) => prev+curr,0) / marks.length)
+// function getAverage(marks){
+//   return Math.floor(marks.reduce((prev,curr) => prev+curr,0) / marks.length)
+// }
+
+
+//
+////
+//////
+//Count the Monkeys!---------------------------------------------------------------------------
+//////
+////
+//
+
+// DESCRIPTION:
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+// For example(Input --> Output):
+
+// 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  1 --> [1]
+
+
+function monkeyCount(n){
+  let newArr = []
+  for(i=1;i<=n; i++){
+    newArr.push(i)
+  }
+  return newArr
 }
+
+
+
+// RECURSIVE
+// let newArr = []
+// function monkeyCount(n, total = 1) {
+//   newArr.push(total)
+//   if(total>=n){
+//     return newArr
+//   }
+//   return monkeyCount(n, total + 1)
+// }
+
+// console.log(monkeyCount(3))
