fe339d58938b324c0b434759f30c4f58de4e91c8
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-29 10:30:05 -0700

    Finish CodeWars 1: Sum of Differences in Array

--- js/8kyu.js
+++ js/8kyu.js
@@ -5407,6 +5407,46 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 
 // For example, when the input is green, output should be yellow.
 
-function updateLight(current) {
-  return current === "green" ? "yellow" : current === "yellow" ? "red" :  "green"
+// function updateLight(current) {
+//   return current === "green" ? "yellow" : current === "yellow" ? "red" :  "green"
+// }
+
+
+
+//
+////
+//////
+//Sum of differences in array-------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+
+function sumOfDifferences(arr) {
+  let sortedArr = arr.sort((a,b) => b-a )
+  let differences = []
+  sortedArr.forEach((number,index) => {
+    let num = number - sortedArr[index+1]
+    if (isNaN(num) === false){
+      differences.push(num)    
+  }})
+  return differences.reduce((curr, prev) => curr + prev, 0)
 }
+
+// function sumOfDifferences(arr) {
+//   return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+// }
+
+console.log(sumOfDifferences([2,1,10]))
