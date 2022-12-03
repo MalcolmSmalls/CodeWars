36875c014717651610dbfb98542b657a9f9ba702
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-03 11:01:02 -0800

    Finish Codewars 4: Grasshopper - Messi Goals

--- js/8kyu.js
+++ js/8kyu.js
@@ -5734,6 +5734,34 @@ const bonusTime = (salary, bonus) => bonus === true ? `\u00A3`+salary * 10 : `\u
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
-function hoopCount (n) {
-  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it" 
-}
+// function hoopCount (n) {
+//   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it" 
+// }
+
+
+//
+////
+//////
+//Grasshopper - Messi Goals---------------------------------------------------------------------------------
+//////
+////
+//
+
+
+// DESCRIPTION:
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
