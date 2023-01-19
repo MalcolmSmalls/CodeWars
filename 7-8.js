0b2d0283bc22c923e9c7dd829c12cc6f74a61346
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-18 23:32:11 -0800

    Finish CodeWars 2: Price of Mangoes

--- 7-8.js
+++ 7-8.js
@@ -1,40 +1,47 @@
 // Trimming a string
 
+/* 
 
-function trim(str, size){
-    const arr = str.split('')
-    return arr.length <= size ? str : arr.length <= 3 ? arr.slice(0, size).join('') + '...' : arr.slice(0, size-3).join('') + '...'   
-}
 
-// function trim(str, size) {
-//     const array = str.split('')
-//     let newArr = []
-//     if(array.length <= size){
-//         return str
-//     } else if(array.length <= 3){
-//         for(let i = 0; i < size+3; i++){
-//             if(i < size){
-//                 newArr.push(array[i])
-//             }else{
-//                 newArr.push('.')
-//             }
-//         }
-//     } else {
-//         for(let i = 0; i <= size-1; i++){
-//             if(i <= size-4){
-//                 newArr.push(array[i])
-//             }else{
-//                 newArr.push('.')
-//             }
-    
-    
-//         }
-        
-//     }
 
-//     return newArr.join('')
+*/
+
+
+// function trim(str, size){
+//     const arr = str.split('')
+//     return arr.length <= size ? str : arr.length <= 3 ? arr.slice(0, size).join('') + '...' : arr.slice(0, size-3).join('') + '...'   
 // }
 
-console.log(trim("Creating kata is fun", 14))
+// console.log(trim("Creating kata is fun", 14))
 
+
+
+
+
+
+
+// Price of Mangoes
+
+/* 
+
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+Examples
+mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
+
+*/
+
+function mango(quantity, price) {
+    return price * (quantity - Math.floor(quantity / 3))
+}
+
+
+console.log(mango(2, 3)) 
+console.log(mango(3, 3))
+console.log(mango(5, 3))
+console.log(mango(9, 5))
 
