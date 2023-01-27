a791410f9d92901c8d82c308066985f2b8fc8fb2
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-27 10:30:49 -0800

    Add pure recursion solution

--- Recursion.js
+++ Recursion.js
@@ -15,3 +15,33 @@ function countDown(num) {
 }
 
 countDown(5)
+
+/*
+
+Add up to a number recursively
+
+*/
+
+function sumRange(num) {
+  if (num === 0) {
+    return num
+  }
+  return num + sumRange(num - 1)
+}
+
+console.log(sumRange(5))
+
+function collectOddValues(arr) {
+  let newArr = []
+
+  if (arr.length === 0) {
+    return newArr
+  }
+
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0])
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)))
+  return newArr
+}
