48fb9ea65e4ae9c2234aa3379c8d6854c4c061df
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-27 11:01:43 -0800

    Finish productOfArray challenge

--- Recursion.js
+++ Recursion.js
@@ -63,11 +63,20 @@ function collectOddValues(arr) {
 
 // console.log(power(2, 4))
 
-function factorial(num) {
-  if (num === 0) {
+// function factorial(num) {
+//   if (num === 0) {
+//     return 1
+//   }
+//   return num * factorial(num - 1)
+// }
+
+// console.log(factorial(4))
+
+function productOfArray(arr) {
+  if (arr.length === 0) {
     return 1
   }
-  return num * factorial(num - 1)
+  return arr[0] * productOfArray(arr.slice(1))
 }
 
-console.log(factorial(4))
+console.log(productOfArray([1, 2, 3, 10]))
