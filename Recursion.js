7502206324a83f0033323950947d2c64f4463b29
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-27 11:35:51 -0800

    Finish recursiveRange challenge

--- Recursion.js
+++ Recursion.js
@@ -72,11 +72,20 @@ function collectOddValues(arr) {
 
 // console.log(factorial(4))
 
-function productOfArray(arr) {
-  if (arr.length === 0) {
-    return 1
+// function productOfArray(arr) {
+//   if (arr.length === 0) {
+//     return 1
+//   }
+//   return arr[0] * productOfArray(arr.slice(1))
+// }
+
+// console.log(productOfArray([1, 2, 3, 10]))
+
+function recursiveRange(num) {
+  if (num === 0) {
+    return num
   }
-  return arr[0] * productOfArray(arr.slice(1))
+  return num + recursiveRange(num - 1)
 }
 
-console.log(productOfArray([1, 2, 3, 10]))
+console.log(recursiveRange(10))
