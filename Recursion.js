c0a8200735c5cb8d9d4d44c6f0ca46598c21c799
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-28 11:27:48 -0800

    Finish capitalizeFirst recursive challenge

--- Recursion.js
+++ Recursion.js
@@ -221,3 +221,27 @@ function flatten(arr) {
 }
 
 console.log(flatten([1, [2, [3, 4], [[5]]]]))
+
+/* 
+
+capitalizeFirst
+Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+
+
+*/
+
+function capitalizeFirst(arr) {
+  let newArr = []
+  if (arr.length === 0) {
+    return newArr
+  }
+  newArr.push(arr[0][0].toUpperCase() + arr[0].substring(1))
+  newArr = newArr.concat(capitalizeFirst(arr.slice(1)))
+  return newArr
+}
+
+console.log(capitalizeFirst(['car', 'taco', 'banana']))
+
+// const newArr = ['car', 'taco']
+
+// console.log(newArr[0][0])
