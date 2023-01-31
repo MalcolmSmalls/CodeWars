3a5336ba8ce1d4e0cfe84cd6df52338af12af9b6
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-31 14:03:41 -0800

    Add other solution to mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -25,12 +25,41 @@ function merge(arr1, arr2) {
 }
 
 function mergeSort(arr) {
-  let mid = Math.floor(arr.length / 2)
-  let left = arr.slice(0, mid)
-  let right = arr.slice(mid)
-  if (arr.length === 1) {
+  if (arr.length <= 1) {
     return arr
   }
-  return merge(mergeSort(left), mergeSort(right))
+  let middle = Math.floor(arr.length / 2)
+  let left = mergeSort(arr.slice(0, middle))
+  let right = mergeSort(arr.slice(middle))
+  return merge(left, right)
 }
-console.log(mergeSort([4, 3, 1, 8, 10, 20, 2, 5]))
+
+console.log(mergeSort([3, 2, 4, 6, 19, 10, 58, 81, 6]))
+
+// Another Way
+
+// function merge (left, right) {
+//     let newArr = []
+//     while(left.length > 0 && right.length > 0){
+//         if(left[0] < right[0]) {
+//             newArr.push(left.shift())
+//         }else{
+//             newArr.push(right.shift())
+//         }
+
+//     }
+
+//     return [...newArr, ...left, ...right]
+
+// }
+
+// function mergeSort(arr) {
+//   let mid = Math.floor(arr.length / 2)
+//   let left = arr.slice(0, mid)
+//   let right = arr.slice(mid)
+//   if (arr.length <= 1) {
+//     return arr
+//   }
+//   return merge(mergeSort(left), mergeSort(right))
+// }
+// console.log(mergeSort([4, 3, 1, 8, 10, 20, 2, 5]))
