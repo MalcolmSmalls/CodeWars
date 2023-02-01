aa5c3fab475939ef0bebe7f42933712e81c8a1ae
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-01 13:43:31 -0800

    Add pop method to SinglyLinkedList

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -11,22 +11,36 @@ class LinkedList {
     this.length = 0
   }
   push(val) {
-    let temp = new Node(val)
-    if (this.length === 0) {
-      this.tail = temp
-      this.head = this.tail
-      this.length++
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
     } else {
-      this.tail.next = temp
-      this.tail = temp
-      this.length++
+      this.tail.next = newNode
+      this.tail = newNode
     }
+    this.length++
+    return this
+  }
+  pop() {
+    let current = this.head
+    for (let i = 1; i < this.length; i++) {
+      if (!this.head && !this.tail) return undefined
+      if (i === this.length - 1) {
+        current.next = null
+        this.tail = current
+        this.length--
+      } else {
+        current = current.next
+      }
+    }
+    return this
   }
 }
 
 const firstLinkedList = new LinkedList()
 firstLinkedList.push('Hi')
-firstLinkedList.push('How')
-firstLinkedList.push('are')
 firstLinkedList.push('You')
+firstLinkedList.push('How')
 console.log(firstLinkedList)
+console.log(firstLinkedList.pop())
