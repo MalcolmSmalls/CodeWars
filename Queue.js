d686b2491ea55d8cb2cdd9c8e05e8d51f6cea01f
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-05 12:22:54 -0800

    Add enqueue method

--- Queue.js
+++ Queue.js
@@ -11,4 +11,17 @@ class Queue {
     this.last = null
     this.size = 0
   }
+
+  enqueue(val) {
+    let node = new Node(val)
+    if (!this.last) {
+      this.last = node
+      this.first = node
+    } else {
+      this.last.next = node
+      this.last = node
+    }
+    this.size++
+    return this.size
+  }
 }
