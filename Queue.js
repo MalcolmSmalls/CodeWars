0f05deab8fdcb40c79470bde288105cb1a2d1d7f
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-05 12:24:52 -0800

    Add dequeue method

--- Queue.js
+++ Queue.js
@@ -24,4 +24,28 @@ class Queue {
     this.size++
     return this.size
   }
+
+  dequeue() {
+    if (!this.first) return null
+    let removedNode = this.first
+    if (this.size === 1) {
+      this.first = null
+      this.last = null
+    } else {
+      this.first = removedNode.next
+      removedNode.next = null
+    }
+    this.size--
+    return removedNode
+  }
 }
+
+const queueDawg = new Queue()
+
+console.log(queueDawg.enqueue('first'))
+console.log(queueDawg.enqueue('second'))
+console.log(queueDawg.enqueue('third'))
+
+console.log(queueDawg.dequeue('first'))
+console.log(queueDawg.dequeue('second'))
+console.log(queueDawg.dequeue('third'))
