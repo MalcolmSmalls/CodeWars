de1b3f97a12b3a9d44984580e708668013468d8b
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-05 11:16:47 -0800

    Add push method to Stack class

--- StacksAndQueues.js
+++ StacksAndQueues.js
@@ -1,14 +1,33 @@
-class Stack {
-  constructor() {
-    this.first()
-    this.last()
-    this.size = 0
-  }
-}
-
 class Node {
   constructor(value) {
     this.value = value
     this.next = null
   }
 }
+
+class Stack {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+
+  push(val) {
+    let node = new Node(val)
+    if (!this.first) {
+      this.first = node
+      this.last = node
+    } else {
+      let current = this.first
+      this.first = node
+      node.next = current
+    }
+    return ++this.size
+  }
+
+  pop() {}
+}
+
+let fatStax = new Stack()
+console.log(fatStax.push('blockin'))
+console.log(fatStax.push('baller'))
