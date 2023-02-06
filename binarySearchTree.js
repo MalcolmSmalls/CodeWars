fb6c029708573cd014c25ba64bf2a6325f173afe
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-06 12:03:48 -0800

    Add insert method to binarySearchTree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
+  insert(value) {
+    let newNode = new Node(value)
+    if (!this.root) {
+      this.root = newNode
+      return this
+    } else {
+      let current = this.root
+      while (true) {
+        if (value === current.value) return undefined
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode
+            return this
+          }
+          current = current.left
+        } else {
+          if (!current.right) {
+            current.right = newNode
+            return this
+          }
+          current = current.right
+        }
+      }
+    }
+  }
 }
+
+const bts = new BinarySearchTree()
+
+console.log(bts.insert(12))
+console.log(bts.insert(911))
+console.log(bts.insert(90210))
