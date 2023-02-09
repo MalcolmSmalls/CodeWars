2ddb7f7adf288b93bc5a1a58337f9ae4102ffcd6
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-08 18:32:48 -0800

    Add set, get, values and keys methods for HashTable

--- HashTable.js
+++ HashTable.js
@@ -1,5 +1,5 @@
 class HashTable {
-  constructor(size = 53) {
+  constructor(size = 3) {
     this.keyMap = new Array(size)
   }
 
@@ -13,4 +13,65 @@ class HashTable {
     }
     return total
   }
+
+  set(key, value) {
+    let index = this._hash(key)
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = []
+    }
+    this.keyMap[index].push([key, value])
+  }
+
+  get(key) {
+    let index = this._hash(key)
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1]
+        }
+      }
+    }
+    return undefined
+  }
+
+  keys() {
+    let keyArr = []
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keyArr.includes(this.keyMap[i][j][0])) {
+            keyArr.push(this.keyMap[i][j][0])
+          }
+        }
+      }
+    }
+    return keyArr
+  }
+
+  values() {
+    let valArr = []
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valArr.includes(this.keyMap[i][j][1])) {
+            valArr.push(this.keyMap[i][j][1])
+          }
+        }
+      }
+    }
+    return valArr
+  }
 }
+
+const hb = new HashTable()
+
+hb.set('boosie', 'i like it')
+hb.set('moneyman', 'make sure you do it big')
+hb.set('jayz', 'i dumb down for my audience and double my dollas')
+hb.set('biggie', 'make sure you do it big')
+
+// console.log(hb.keyMap)
+console.log(hb.values())
+console.log(hb.keys())
