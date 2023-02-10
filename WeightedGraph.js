3d1e60f40efaf7fb766f48a68b6b7505ee3541c4
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-10 10:51:44 -0800

    Add priorityQueue

---
+++ WeightedGraph.js
@@ -0,0 +1,36 @@
+class PriorityQueue {
+  constructor() {
+    this.values = []
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority })
+    this.sort()
+  }
+  dequeue() {
+    return this.values.shift()
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority)
+  }
+}
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight })
+    this.adjacencyList[vertex2].push({ node: vertex1, weight })
+  }
+}
+
+let newGraph = new WeightedGraph()
+
+newGraph.addVertex('A')
+newGraph.addVertex('B')
+newGraph.addEdge('A', 'B', 20)
+
+console.log(newGraph.adjacencyList)
