30e20b3160f90fcd976124b8ca417e04a78c21ad
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-18 12:15:52 -0800

    Add flatten solution

--- Anki.js
+++ Anki.js
@@ -52,3 +52,17 @@ function longestSubString(str) {
   }
   return longest
 }
+
+function flatten(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      newArr.push(arr[i])
+    } else {
+      newArr = newArr.concat(flatten(arr[i]))
+    }
+  }
+  return newArr
+}
+
+console.log(flatten([1, 2, 3, [4, 5]]))
