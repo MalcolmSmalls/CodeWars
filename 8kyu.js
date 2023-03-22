94dae13a28f178d01c7acea15d13a04e5730b18f
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-22 13:56:06 -0700

    Finish Unfinished Loop - Bug Fixing #1

--- 8kyu.js
+++ 8kyu.js
@@ -182,3 +182,15 @@ function booleanToString(b) {
 function mango(quantity, price) {
   return (quantity - Math.floor(quantity / 3)) * price
 }
+
+// Unfinished Loop - Bug Fixing #1
+
+function createArray(number) {
+  var newArray = []
+
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter)
+  }
+
+  return newArray
+}
