1f5ce87b70290e7280bee7674f5c50c9f0c4a581
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-22 13:38:26 -0700

    Finish Fun with ES6 Classes #1 - People, people, people

--- 8kyu.js
+++ 8kyu.js
@@ -140,3 +140,20 @@ function Hero(name) {
   this.damage = 5
   this.experience = 0
 }
+
+//Fun with ES6 Classes #1 - People, people, people
+
+class Person {
+  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.gender = gender
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`
+  }
+}
