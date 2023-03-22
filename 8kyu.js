03aefc39b0066234991ed478710c161dd61f9da8
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-22 13:29:22 -0700

    Finish Grasshopper - Terminal Game #1

--- 8kyu.js
+++ 8kyu.js
@@ -130,3 +130,13 @@ function mystery() {
   var results = { sanity: 'Hello' }
   return results
 }
+
+// Grasshopper - Terminal Game #1
+
+function Hero(name) {
+  this.name = name || 'Hero'
+  this.position = '00'
+  this.health = 100
+  this.damage = 5
+  this.experience = 0
+}
