860c918886a9017895cb6a703e98d41b733e832d
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-22 23:22:24 -0700

    Finish Find Maximum and Minimum Values of a List

--- 8kyu.js
+++ 8kyu.js
@@ -375,3 +375,15 @@ function helloWorld() {
 function none(arr, fun) {
   return !arr.some(fun)
 }
+
+// Find Maximum and Minimum Values of a List
+
+var min = function (list) {
+  list.sort((a, b) => a - b)
+  return list[0]
+}
+
+var max = function (list) {
+  list.sort((a, b) => b - a)
+  return list[0]
+}
