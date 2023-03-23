6963df8399e11c2ca6ec5677c9ccc92974d23378
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-23 13:30:54 -0700

    Finish Welcome!

--- 8kyu.js
+++ 8kyu.js
@@ -581,3 +581,33 @@ function getAverage(marks) {
 
   return Math.floor(total / marks.length)
 }
+
+// Welcome!
+
+function greet(language) {
+  const database = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  }
+  for (let key in database) {
+    if (key === language) {
+      return database[language]
+    }
+  }
+  return 'Welcome'
+}
