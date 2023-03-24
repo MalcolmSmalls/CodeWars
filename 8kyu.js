348e0bad1f7b0e42abdd50b0cab429497088ba37
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-23 19:13:43 -0700

    Finish You Can't Code Under Pressure #1

--- 8kyu.js
+++ 8kyu.js
@@ -694,3 +694,8 @@ function quadratic(x1, x2) {
 function basicOp(operation, value1, value2) {
   return eval(`${value1}${operation}${value2}`)
 }
+
+// You Can't Code Under Pressure #1
+function doubleInteger(i) {
+  return i + i
+}
