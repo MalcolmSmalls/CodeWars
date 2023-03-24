5ca83b7815fcfe3161caaba752349230823e5427
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-23 20:00:36 -0700

    Finish pick a set of first elements

--- 8kyu.js
+++ 8kyu.js
@@ -753,3 +753,8 @@ function Ship(draft, crew) {
 function makeUpperCase(str) {
   return str.toUpperCase()
 }
+
+// pick a set of first elements
+function first(arr, n = 1) {
+  return arr.slice(0, n)
+}
