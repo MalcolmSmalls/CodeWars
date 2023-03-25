a9ff4b6d35687e84f26b5232416a1893d2f39860
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-25 05:35:04 +0000

    Finish Multiplication table for number

--- 8kyu.js
+++ 8kyu.js
@@ -1072,3 +1072,11 @@ function multipleOfIndex(array) {
   return newArr
 }
 
+// Multiplication table for number
+function multiTable(number) {
+  let str = ''
+  for(let i = 1; i <= 10; i++){
+    str += `${i} * ${number} = ${number * i}\n`
+  }
+  return str.slice(0, str.length-1)
+}
