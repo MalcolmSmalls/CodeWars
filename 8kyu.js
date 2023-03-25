bae18a9935451eaf675b32796832907354e9015c
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-25 03:40:31 +0000

    Finish Training JS #10 loop statement ---for

--- 8kyu.js
+++ 8kyu.js
@@ -1030,3 +1030,19 @@ function giveMeFive(obj){
   }
   return newArr
 }
+
+
+
+// Training JS #10: loop statement --for
+function pickIt(arr){
+  var odd=[],even=[];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      even.push(arr[i])
+    }else{
+      odd.push(arr[i])
+    }
+  }
+  
+  return [odd,even];
+}
