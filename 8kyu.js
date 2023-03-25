0d94e6e01c9db8844769edae19297c8f9ce5ff67
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-25 21:32:03 +0000

    Finish Training JS #8: Conditional statement--switch

--- 8kyu.js
+++ 8kyu.js
@@ -1182,3 +1182,20 @@ function amIWilson(p) {
   const primes = [5,13,563]
   return primes.indexOf(p) >= 0 ? true: false
 }
+
+// Training JS #8: Conditional statement--switch
+function howManydays(month){
+  var days;
+  switch (month){
+  case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+      days = 31
+      break
+  case 4: case  6: case  9: case  11:
+      days = 30
+      break
+  case 2:
+      days = 28
+      break
+  }
+  return days;
+}
