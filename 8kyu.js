0473404560925da1f7ad61fa2f3293b6fa152afb
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-25 08:21:12 +0000

    Finish Pillars

--- 8kyu.js
+++ 8kyu.js
@@ -1151,3 +1151,10 @@ function removeEveryOther(arr){
   })
   return newArr
 }
+
+// Pillars
+
+function pillars(numPill, dist, width) {
+  if(numPill===1) return 0
+  return ((numPill-1) * dist * 100) + ((numPill-2)*width)
+}
