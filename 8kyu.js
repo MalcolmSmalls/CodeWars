a0597670a36cb3656a3675f12792584845b4576f
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-26 07:50:36 +0000

    Finish Reversed sequence

--- 8kyu.js
+++ 8kyu.js
@@ -1570,3 +1570,14 @@ function doubleChar(str) {
   return newStr
   
 }
+
+
+// Reversed sequence
+const reverseSeq = n => {
+  let newArr = []
+  while(n > 0){
+    newArr.push(n)
+    n--
+  }
+  return newArr
+};
