fb897aa4c05f00467e8406df637d3e888f362555
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-26 06:44:36 +0000

    Finish Count by X

--- 8kyu.js
+++ 8kyu.js
@@ -1460,3 +1460,14 @@ websites.push('codewars')
 
 // Mr. Freeze
 Object.freeze(MrFreeze)
+
+
+// Count by X
+
+function countBy(x, n) {
+  let z = [];
+  for(let i = x; i <= n * x; i = i + x){
+    z.push(i)
+  }
+  return z;
+}
