6c2d54076931d80b921a3faaca15ee6a865a4ad6
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-27 05:45:17 +0000

    Finish isReallyNaN

--- 8kyu.js
+++ 8kyu.js
@@ -1700,3 +1700,9 @@ const configuration = {
 function playerRankUp (points) {
   return points >= 100 ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." : false
 }
+
+
+// isReallyNaN
+const isReallyNaN = (val) => {
+  return Number.isNaN(val)
+};
