428a87826f34e33aea92a9a41d7135f55f1d2d1f
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-27 04:02:39 +0000

    Finish Find the smallest integer in the array

--- 8kyu.js
+++ 8kyu.js
@@ -1656,3 +1656,10 @@ function fakeBin(x){
   }
   return newStr
 }
+
+// Find the smallest integer in the array
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort((a,b) => a-b)[0]
+  }
+}
