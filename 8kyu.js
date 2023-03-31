7b6d408b0ce7b6421a9d6ae0a024adb7e85464b5
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-03-31 00:13:33 +0000

    Finish Fundamentals: Return

--- 8kyu.js
+++ 8kyu.js
@@ -1906,3 +1906,30 @@ function stringClean(s){
   return s.replace(reg, '')
 }
 
+
+// Fundamentals: Return
+
+function add(a,b){
+  return a+b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return Math.pow(a, b)
+}
+  
+function subt(a,b){
+  return a-b
+}
+
