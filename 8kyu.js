4d2ef48f263a6c6fd9365e3d54e7ab15ae6f29d8
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-24 18:59:41 +0000

    Age Range Compatibility Equation

--- 8kyu.js
+++ 8kyu.js
@@ -2676,3 +2676,18 @@ class Person {
   }
 
 }
+
+// Age Range Compatibility Equation
+
+function datingRange(age){
+  let min = 0
+  let max = 0
+  if(age > 14){
+    min = Math.floor(age / 2 + 7)
+    max = Math.floor((age - 7) * 2)
+  }else{
+    min = Math.floor(age - 0.10 * age)
+    max = Math.floor(age + 0.10 * age)
+  }
+  return `${min}-${max}`
+}
