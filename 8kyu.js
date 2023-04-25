50581d156c8d65e237163ab0acd3e9b9afea3ea2
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-25 20:50:39 +0000

    Finish Leonardo Dicaprio and Oscars

--- 8kyu.js
+++ 8kyu.js
@@ -2875,3 +2875,18 @@ function fuelPrice(litres, pricePerLitre) {
   }
   return Number(((pricePerLitre - discount) * litres).toFixed(2))
 }
+
+// Leonardo Dicaprio and Oscars
+
+function leo(oscar){
+  switch(true){
+      case oscar === 88:
+        return 'Leo finally won the oscar! Leo is happy'
+      case oscar === 86:
+        return 'Not even for Wolf of wallstreet?!'
+      case oscar > 88:
+        return 'Leo got one already!'
+      case oscar < 88:
+        return 'When will you give Leo an Oscar?'
+  }
+}
