805291fefffff3f834f933ad50d051c98a82ba3c
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-26 23:36:34 +0000

    Finish Training JS #13: Number object and its properties

--- 8kyu.js
+++ 8kyu.js
@@ -2979,3 +2979,15 @@ function colorOf(r,g,b){
 
   return `#${str}`
 }
+
+// Training JS #13: Number object and its properties
+
+function whatNumberIsIt(n){
+  if(n === Number.NEGATIVE_INFINITY) return "Input number is Number.NEGATIVE_INFINITY"
+  if(n === Number.POSITIVE_INFINITY) return "Input number is Number.POSITIVE_INFINITY"
+  if(n >= Number.MAX_VALUE) return "Input number is Number.MAX_VALUE"
+  if(n <= Number.MIN_VALUE && n !== 0) return "Input number is Number.MIN_VALUE"
+  if(isNaN(n)) return "Input number is Number.NaN"
+  return `Input number is ${n}`
+  
+}
