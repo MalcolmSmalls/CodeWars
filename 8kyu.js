b8ffae6963f88a824d9c1eccd23c9c156ca92a6f
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 18:59:12 +0000

    Finish Miles per gallon to kilometers per liter

--- 8kyu.js
+++ 8kyu.js
@@ -3344,3 +3344,10 @@ var greet_ben = function() {
   return "Hello, " + name + '!';
 };
 
+
+
+// Miles per gallon to kilometers per liter
+
+function converter (mpg) {
+  return +((mpg * 1.609344) / 4.54609188).toFixed(2)
+}
