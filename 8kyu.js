b2367d10c69c8d63b819a1d4d3cdc53e521973d2
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 18:40:53 +0000

    Finish Max Headroom and JavaScript style

--- 8kyu.js
+++ 8kyu.js
@@ -3318,3 +3318,15 @@ var Ghost = function() {
   this.colors = ['white', 'yellow', 'purple', 'red']
   this.color = this.colors[Math.floor(Math.random() * 5)]
 };
+
+// Max Headroom and JavaScript style
+function getMax1(){
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2(){
+  return {name: 'Max Headroom'}
+}
+
+
