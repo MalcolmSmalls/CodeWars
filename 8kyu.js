57a2ef9e6680afad513b1f2666b9d3291854bd5e
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 19:16:50 +0000

    Finish How much water do I need?

--- 8kyu.js
+++ 8kyu.js
@@ -3351,3 +3351,10 @@ var greet_ben = function() {
 function converter (mpg) {
   return +((mpg * 1.609344) / 4.54609188).toFixed(2)
 }
+
+// How much water do I need?
+function howMuchWater(water, load, clothes){
+  if(clothes > load * 2) return 'Too much clothes'
+  if(clothes < load) return 'Not enough clothes'
+  return +(water * 1.1 ** (clothes - load)).toFixed(2)
+}
