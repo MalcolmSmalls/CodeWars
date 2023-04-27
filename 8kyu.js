143449cbde437dba4a8948688559e1d8eadb42b3
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 01:08:13 +0000

    Finish Fix your code before the garden dies!

--- 8kyu.js
+++ 8kyu.js
@@ -3075,3 +3075,13 @@ function bloodAlcoholContent(drinks, weight, sex, time){
   const multiplier = sex === 'male' ? .73 : .66
   return Number((((drinks['ounces'] * drinks['abv']) * 5.14 / weight * multiplier) - .015 * time).toFixed(4))
 }
+
+
+// Fix your code before the garden dies!
+function rainAmount(mm){
+  if (mm < 40) {
+       return "You need to give your plant " + (40 - mm) + "mm of water"
+  }else {
+       return "Your plant has had more than enough water for today!"
+  };
+}
