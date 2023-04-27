3887d9c9b55bfe84c535610ae300ece7dfec0a7f
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 05:35:22 +0000

    Finish Grasshopper - Object syntax debug

--- 8kyu.js
+++ 8kyu.js
@@ -3160,3 +3160,21 @@ Array.prototype.filter = function (func) {
   this.forEach(item => func(item) ? newArr.push(item) : null )
   return newArr
 }
+
+// Grasshopper - Object syntax debug
+var rooms = {
+  first: {
+    description: 'This is the first room',
+    items: {
+      chair: 'The old chair looks comfortable',
+      lamp: 'This lamp looks ancient'
+    }
+  },
+  second: {
+    description: 'This is the second room',
+    items: {
+      couch: 'This couch looks like it would hurt your back',
+      table: 'On the table there is an unopened bottle of water'
+    }
+  }
+}
