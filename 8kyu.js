305a854061edaadb6684b75000db1c2394cd595a
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 00:17:11 +0000

    Finish Training JS #4: Basic data types--Array

--- 8kyu.js
+++ 8kyu.js
@@ -3018,3 +3018,24 @@ function howManySmaller(arr,n){
 function greaterThanLessThan (a,b,c) {
   return a < b < c
 }
+
+
+// Training JS #4: Basic data types--Array
+function getLength(arr){
+  return arr.length
+}
+function getFirst(arr){
+  return arr.shift()
+}
+function getLast(arr){
+  return arr.pop()
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  return arr
+}
+function popElement(arr){
+  arr.pop()
+  return arr
+}
