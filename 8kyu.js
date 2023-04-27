5b1c340ca434fb6dfdb7de4bf7d7ba5ed1b6583e
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 18:45:28 +0000

    Finish Shifty Closures

--- 8kyu.js
+++ 8kyu.js
@@ -3330,3 +3330,17 @@ function getMax2(){
 }
 
 
+
+// Shifty Closures
+
+
+var greet_abe = function() {
+  let name = 'Abe'
+  return "Hello, " + name + '!';
+};
+
+var greet_ben = function() {
+  let name = 'Ben';
+  return "Hello, " + name + '!';
+};
+
