9b55405e1369b7d7ef07f41bbcb3f55811875c71
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-04-27 06:56:34 +0000

    Finish Barking mad

--- 8kyu.js
+++ 8kyu.js
@@ -3268,3 +3268,20 @@ function anyArrows(arrows){
 var ArrowFunc = function(arr) {
   return arr.map(element => String.fromCharCode(element) ).join(''); //Complete this function
 }
+
+// Barking mad
+
+function Dog (breed) {
+  this.breed = breed;
+  this.bark = function(){
+    return 'Woof'
+  }
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
