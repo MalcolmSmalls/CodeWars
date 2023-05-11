f876cd3e0296fc274b5ad46afed63e9dc1bd3c12
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-05-11 00:46:35 +0000

    Finish Composing squared strings

--- 7kyu.js
+++ 7kyu.js
@@ -6,3 +6,20 @@ Array.prototype.remove_ = function(integer_list, values_list){
     integer_list.forEach(item => !values_list.includes(item) && newArr.push(item))
     return newArr
   }
+
+
+// Composing squared strings
+  function compose(s1, s2) {
+    console.log(s1, s2)
+    let newS1 = s1.split('\n')
+    let newS2 = s2.split('\n')
+    let str1Chars = 1
+    let str2Chars = newS2[newS2.length-1].length
+    let compoundedStr = ''
+    newS1.forEach((item, index) => {
+      compoundedStr += `${item.slice(0, str1Chars)}${newS2[newS2.length - (1 + index)].slice(0, str2Chars)}\n`
+      str1Chars++
+      str2Chars--
+    })
+  return compoundedStr.slice(0, compoundedStr.length - 1)
+}
