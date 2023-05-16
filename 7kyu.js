401359cb01b0146ce8b056074a02fd75acaea632
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-05-16 19:56:03 +0000

    Finish Is this a triangle?

--- 7kyu.js
+++ 7kyu.js
@@ -392,3 +392,9 @@ function validatePIN (pin) {
 function addBinary(a,b) {
   return (a+b).toString(2)
 }
+
+// Is this a triangle?
+function isTriangle(a,b,c){
+  if(a+b > c && a+c > b && b+c > a) return true
+  return false
+}
