a656175314a15187afdb1aa8c33e084d6828ffec
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-05-16 18:23:14 +0000

    Finish Find the next perfect square!

--- 7kyu.js
+++ 7kyu.js
@@ -355,3 +355,11 @@ function openOrSenior(data){
   })
   return newArr
 }
+
+// Find the next perfect square!
+function findNextSquare(sq) {
+  if(Number.isInteger(Math.sqrt(sq))){
+    return (Math.sqrt(sq) + 1) ** 2
+  }
+  return -1;
+}
