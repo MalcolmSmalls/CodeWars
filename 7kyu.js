86934c7b6915b3760f9305182e1263438fd76494
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-05-16 01:43:38 +0000

    Finish Sum of two lowest positive integers

--- 7kyu.js
+++ 7kyu.js
@@ -311,3 +311,9 @@ function maskify(cc) {
   }
   return cc
 }
+
+// Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {  
+  numbers.sort((a,b) => a-b)
+  return numbers[0] + numbers[1]
+}
