576f5233be5b74597f1e6a61ec640464a405161e
Malcolm Smalls <42323966+MalcolmSmalls@users.noreply.github.com>
2023-07-02 10:22:23 +0000

    Finish Check the exam

--- 7kyu.js
+++ 7kyu.js
@@ -712,3 +712,18 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
   }
   return day
 }
+
+
+// Check the exam
+
+
+function checkExam(array1, array2) {
+  let score = 0
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] === array2[i]){
+      score += 4
+    }else if(array1[i] !== array2[i] && array2[i])
+      score -= 1
+  }
+  return score > 0 ? score : 0
+}
